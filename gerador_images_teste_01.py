9529e2f65fa07f77506697ebd894bb7c675db506
Lucas <lucasjsbatista@gmail.com>
2023-08-25 23:14:16 -0400

    New files for graph plotting

--- gerador_images_teste_01.py
+++ gerador_images_teste_01.py
@@ -4,5 +4,8 @@ altitudes = [0.0, -0.105, -0.178, 0.366, 1.647, 2.311, 2.258, 1.815, 1.323, 0.82
 seconds = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5]
 y_points = [y for y in altitudes]
 x_points = [x for x in seconds]
-plt.plot(x_points, y_points, 'bo', linestyle="-", scalex=100, scaley=100)
+plt.plot(x_points, y_points, 'bo', linestyle="-")
+plt.grid(color='b', linestyle='-', linewidth=0.1)
+plt.xlabel("Tempo (segundos)")
+plt.ylabel("Altitude (metros)")
 plt.savefig('teste_01_altitude.png')
